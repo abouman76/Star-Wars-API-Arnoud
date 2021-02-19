c7aa1050782d2b75c6488db66214bb6e58eadb7d
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-02-19 16:23:54 +0100

    input and error handling

--- main.js
+++ main.js
@@ -8,44 +8,75 @@
 // 7. [x] AXIOS script linken.
 // 8. [x] Een async function maken en **aanroepen**!
 // 9. [x] Request maken met AXIOS.
-// 10. [ ] Afwachten tot de data er is.
-// 11. [ ] Data weergeven in de DOM.
-//  - [ ] Lijst maken met een id.
-//  - [ ] Lijst selecteren.
-//   - [ ] Voor elke element in de array.
-//       - [] Een li aanmaken.
-//       - [ ] textContent toevoegen.
-//        - [ ] li appenden.
+// 10. [x] Afwachten tot de data er is.
+// 11. [x] Data weergeven in de DOM.
+//  - [x] Lijst maken met een id.
+//  - [x] Lijst selecteren.
+//   - [x] Voor elke element in de array.
+//       - [x] Een li aanmaken.
+//       - [x] textContent toevoegen.
+//        - [x] li appenden.
 
-async function fetchDataStarWars() {
-    const response = await axios.get(`https://swapi.dev/api/people/`);
-    console.log("RESPONSE", response);
-    // console.log("Response DATA", response.data);
-    const starWarsData = response.data.results;
-    console.log("Data", starWarsData);
+const searchButton = document.getElementById("button-element");
+searchButton.addEventListener("click", fetchDataStarWars);
 
-    try {
-        const names = document.getElementById("name-result");
-        // console.log(names);
+const personInfo = document.getElementById("character-container");
 
-        for (const character of starWarsData) {
-            // console.log("1 actor at a time?", character);
+const searchInput = document.getElementById("input-element");
+searchInput.addEventListener("keypress", handleKeyPress);
 
-            const nameAndHeight = `${character.name} is one of Star Wars characters and his or her height is ${character.height} cm.`
-            // console.log("NAME and HEIGHT", nameAndHeight);
-
-            const characterInfo = document.createElement("li");
-            characterInfo.textContent = nameAndHeight;
-            console.log("INFO", characterInfo);
-
-            names.appendChild(characterInfo);
+function handleKeyPress(event) {
+    // console.log("KEY PRESSED?", event.code, event.code === "Enter");
+    if (event.code === "Enter") {
+        fetchDataStarWars();
+    }
+}
+    async function fetchDataStarWars() {
+        const outComeElement = searchInput.value;
+        // console.log("OUTCOME", outComeElement, !outComeElement);
+        if(!outComeElement) {
+            return
         }
 
-    }
+        const errorMessage = document.getElementById("error-msg");
+        errorMessage.textContent = "";
 
-    catch(error) {
-        console.log("ERROR");
-    };
+        const response = await axios.get(`https://swapi.dev/api/people?search=${outComeElement}`);
+         console.log("RESPONSE", response, response.data.count === 0 );
+         if(response.data.count === 0){
+             return errorMessage.textContent = `This character does not exists in Star Wars. Try again!`;
+         }
+
+
+        // console.log("Response DATA", response.data);
+
+        let starWarsData = response.data.results;
+        // console.log("Data", starWarsData);
+
+        searchInput.value = "";
+
+        try {
+            // const names = document.getElementById("name-result");
+            // console.log(names);
+
+            for (const character of starWarsData) {
+                // console.log("1 actor at a time?", character);
+
+                const nameAndHeight = `${character.name} is one of Star Wars characters and his or her height is ${character.height} cm.`
+                // console.log("NAME and HEIGHT", nameAndHeight);
+
+                const characterInfo = document.createElement("li");
+                characterInfo.textContent = nameAndHeight;
+                console.log("INFO", characterInfo);
+
+                personInfo.appendChild(characterInfo);
+            }
+
+        } catch (error) {
+            console.log(error);
+            // errorMessage.textContent = `This character does not exists in Star Wars. Try again!`;
+        };
 
 }
-fetchDataStarWars();
+
+// fetchDataStarWars();
